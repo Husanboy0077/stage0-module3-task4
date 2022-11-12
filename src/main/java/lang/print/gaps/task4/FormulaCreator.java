2b617a5664ed815de63217d960db7ee9e016d6d2
Husanboy <olimboyevhusanboy@gmail.com>
2022-11-12 19:18:34 +0500

    donkey

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -7,7 +7,7 @@ public class FormulaCreator {
         double x,a,b;
         a=sc.nextDouble();
         b=sc.nextDouble();
-        x=(9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
+        x=(9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
         System.out.println(x);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,10 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        int c='c';
-        int h='h';
-        int a='a';
-        int r='r';
-        System.out.println(c + h + a + r);
+        char c='c';
+        char h='h';
+        char a='a';
+        char r='r';
+        System.out.print(c + h + a + r );
     }
 }
