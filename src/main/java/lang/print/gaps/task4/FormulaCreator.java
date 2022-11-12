f477a3c683f9e1c7537b9e7879b9a2aa30334e64
Husanboy <olimboyevhusanboy@gmail.com>
2022-11-12 19:23:10 +0500

    donkey

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -4,10 +4,10 @@ public class FormulaCreator {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
-        double x,a,b;
+        double a,b;
         a=sc.nextDouble();
         b=sc.nextDouble();
-        x=(9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
-        System.out.println(x);
+
+        System.out.println((9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2));
     }
 }
