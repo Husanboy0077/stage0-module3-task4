c8131d74097016357b4e1a429738d948924dfc58
Husanboy <olimboyevhusanboy@gmail.com>
2022-11-12 18:52:00 +0500

    donkey

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.task4;
-
+import java.util.Scanner;
 public class FormulaCreator {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        double x,a,b;
+        a=sc.nextDouble();
+        b=sc.nextDouble();
+        x=(9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
+        System.out.println(x);
     }
 }
