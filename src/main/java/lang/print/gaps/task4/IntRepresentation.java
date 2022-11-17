a3ab71a9f18ce486c153b2d7f45ba14e914c741e
Husanboy <olimboyevhusanboy@gmail.com>
2022-11-17 10:54:05 +0500

    donkey

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,10 +2,11 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        char c='c';
-        char h='h';
-        char a='a';
-        char r='r';
-        System.out.print(c + h + a + r );
+        char c=99;
+        char h=104;
+        char a=97;
+        char r=114;
+        System.out.print(c + "" + h + "" + a + "" + r );
+
     }
 }
